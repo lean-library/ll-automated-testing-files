595dbce79a1b668a7533998544ed59fd0c31b764
Robert Jones <robert.jones@sagepub.co.uk>
2024-09-18 16:22:39 +0100

    LL-0 at Cleanup duplicated files

--- firefox-testing/user.js
+++
@@ -1,4 +0,0 @@
-user_pref("devtools.debugger.remote-enabled", true);
-user_pref("devtools.debugger.prompt-connection", false);
-user_pref("extensions.manifestV3.enabled", true);
-user_pref("devtools.chrome.enabled", true);
